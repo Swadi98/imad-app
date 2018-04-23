4a83a35705becea52e4a1e1c6e641ee28e74cd9e
Swadi98 <31335049+Swadi98@users.noreply.github.com>
2018-04-23 19:03:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,8 @@ button.onclick =function(){
 
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginLeft= '100px';
+}
