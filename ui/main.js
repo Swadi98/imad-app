ba30c1052aebe1bd38745494b846f743b1a2710e
Swadi98 <31335049+Swadi98@users.noreply.github.com>
2018-04-23 19:11:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
     marginLeft = marginLeft +10;
-    img.set.marginLeft = marginLeft + 'px';
+    img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick=function(){
