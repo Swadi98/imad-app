ad45bffabcb92a4c052ed4513e70b39f0b6f7d06
Swadi98 <31335049+Swadi98@users.noreply.github.com>
2018-04-25 08:47:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,8 +21,7 @@ button.onclick= function(){
     
 };
 
-var nameInput= document.getElementById('name');
-var name =nameInput.value;
+
 var submit=document.getElementById("submit_btn ");
 submit.onclick= function(){
     
@@ -43,7 +42,8 @@ submit.onclick= function(){
         }
          
     };
-    
+  var nameInput= document.getElementById('name');
+  var name =nameInput.value;  
   request.open('GET','http://kswadi01.imad.hasura-app.io/submit-name?name='+name,true);
   request.send(null);
   
