569086a4f850169b3cdaae40db4bf13d6eddeda2
Swadi98 <31335049+Swadi98@users.noreply.github.com>
2018-04-25 06:47:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 //console.log('Loaded!');
 var button= document.getElementById('counter');
 
-button.onclick= function(){
+/* button.onclick= function(){
     
     var request = new XMLHttpRequest(); 
     request.onreadystatechange= function(){
@@ -20,27 +20,15 @@ button.onclick= function(){
   
     
 };
-
-
-/*button.onclick =function(){
-    
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function(){
-        
-        if(request.readyState === XMLHtttpRequest.DONE){
-            if(request.status ===200){
-                var counter = request.responseText;
-                 var span= document.getElementById('count');
-                 span.innerHTML= counter.toString();
-                
-            }        }
-    };
-
-    request.open('GET', 'http://kswadi01.imad.hasura-app.io/counter',true);
-    request.send(null);
-}; 
 */
+var counter =0;
+button.onclick=function(){
+    counter =counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML = counter.toString();
+}
+
+
 /* var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 
